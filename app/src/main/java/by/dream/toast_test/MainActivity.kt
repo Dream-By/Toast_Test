b1263dff9bb85660140dbc8672206e6fddc78f44
dzmitry.dzianisau@gmail.com <Dream-By>
2020-03-12 10:20:36 +0300

    Update project

--- app/src/main/java/by/dream/toast_test/MainActivity.kt
+++ app/src/main/java/by/dream/toast_test/MainActivity.kt
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,9 +16,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val button: Button = findViewById(R.id.button)
+
         button.setOnClickListener{
+
             val toast = Toast.makeText(applicationContext,"Пора покормить кота!",Toast.LENGTH_LONG)
             toast.setGravity(Gravity.CENTER,0,0)
+
+            val toastContainer = toast.view as LinearLayout
+            val catImage = ImageView(this)
+            catImage.setImageResource(R.drawable.hungry_cat)
+            toastContainer.addView(catImage,0)
             toast.show();
         }
     }
